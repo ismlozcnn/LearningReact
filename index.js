60bac6515bfeddbeb1a1a31086e82104c55498ff
İsmail Özcan <l5215067@std.yildiz.edu.tr>
2022-10-19 20:02:18 +0300

    1.6 finished

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Merabalar efenim")
+import deneme, {topla,cikar,user,users} from  './my-module.js'
+
+console.log(topla(2,4));
+deneme("babacım");
+console.log(cikar(4,2));
+console.log(user.surname)
+console.log(users)
---
+++ my-module.js
@@ -0,0 +1,27 @@
+const hello= message  =>console.log("Hello "+message)
+
+const topla = (a, b) =>  a + b;
+const cikar = (a, b) =>  a - b;
+const user={
+    name:'Ali',
+    surname:'Baba'
+}
+ const users=[
+    {
+        name:'Gökhan',
+        surname:'Ayyıldız'
+    },
+    {
+        name: 'Furkan',
+        surname: 'Akbal'
+    }
+]
+export{
+    topla,
+    cikar,
+    user,
+    users
+};
+
+
+export default hello;
